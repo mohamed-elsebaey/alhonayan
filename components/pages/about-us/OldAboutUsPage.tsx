c535d413cb99521554708c7d178f439f9b67b47d
Mohamed <elsebaey95@outlook.com>
2025-07-09 15:09:58 +0300

    update About Us Images

--- components/pages/about-us/OldAboutUsPage.tsx
+++ components/pages/about-us/OldAboutUsPage.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
 import React from "react";
 import logoAndTitleImage from "@/public/logo/logo-title.png";
-import aboutUsImage from "@/public/images/missionAndVision-image7.png";
+import aboutUsImage1 from "@/public/images/aboutUs-image1.jpg";
+import aboutUsImage2 from "@/public/images/aboutUs-image2.jpg";
 import backgroundImage from "@/public/background/background-image3.png";
 import MissionAndVision from "@/components/sections/missionAndVision/MissionAndVision";
 
@@ -17,7 +18,7 @@ const AboutUsPage = () => {
         priority={true} 
         className="-z-10" 
       />
-      <div className="content-width gap-16 items-start py-8 lg:grid lg:grid-cols-2 lg:py-16">
+      <div className="content-width gap-16 items-center py-8 lg:grid lg:grid-cols-2 lg:py-16">
         <div className="flex flex-col gap-8 items-center lg:items-start">
           <div className="my-2 w-[80%] max-w-[500px]">
             <Image
@@ -29,7 +30,7 @@ const AboutUsPage = () => {
             />
           </div>
           <p className="lg:text-start text-center">
-            تأسست شركة <strong>دار الحنيان للإستشارات الهندسية</strong> منذ عام{" "}
+            تأسس مكتب <strong>دار الحنيان للإستشارات الهندسية</strong> منذ عام{" "}
             <strong>1435 هجرية</strong> ومنذ ذلك الحين ونحن نعمل في مجال الهندسة
             المدنية والمعمارية والمساحية ومشاريع الدراسات والإشراف. تم خلال تلك
             الفترة تكوين كادر هندسي قوي وذو خبرة عالية، وتم تقديم الكثير من
@@ -42,13 +43,21 @@ const AboutUsPage = () => {
             نعمل بمرونة وسرعة لتقديم أفضل الحلول الهندسية.
           </p>
         </div>
-        <div className="mt-8 lg:mt-0 flex justify-center">
+        <div className="grid grid-cols-2 gap-4 mt-8">
           <Image
-            className="max-w-[500px] w-full rounded-lg"
-            src={aboutUsImage.src}
-            alt="aboutUsImage"
-            width={800}
-            height={1000}
+            className="w-full rounded-lg"
+            src={aboutUsImage2.src}
+            alt="aboutUsImage2"
+            width={1700}
+            height={2000}
+            priority={true}
+          />
+          <Image
+            className="mt-4 w-full lg:mt-10 rounded-lg"
+            src={aboutUsImage1.src}
+            alt="aboutUsImage1"
+            width={1700}
+            height={2000}
             priority={true}
           />
         </div>
